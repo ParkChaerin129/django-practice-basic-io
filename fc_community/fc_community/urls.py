b903587778650ec8f005d45ab46156e296c53264
Chaerin Park <alicx991117@naver.com>
2021-03-19 20:03:36 +0900

    게시글 리스트 만들기

--- fc_community/fc_community/urls.py
+++ fc_community/fc_community/urls.py
@@ -19,6 +19,7 @@ from fcuser.views import home
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('fcuser/', include('fcuser.urls')),
+    path('board/', include('board.urls')),
     path('', home),
 
 ]
